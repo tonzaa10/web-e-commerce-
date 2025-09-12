2eb677fae25b9f93b0fc2ee4f898049a5c207374
wittaya <w.imlukin@hotmail.com>
2025-09-12 22:16:57 +0700

    Function gitCartTage

---
+++ src/features/carts/db/cache.ts
@@ -0,0 +1,11 @@
+import { getUserIdTag } from "@/features/users/db/cache";
+import { revalidateTag } from "next/cache";
+
+export const getCartTag = (userId: string) => {
+  return `cart:${userId}` as const;
+};
+
+export const revalidateCartCache = (userId: string) => {
+  revalidateTag(getCartTag(userId));
+  revalidateTag(getUserIdTag(userId))
+};
