8a7f82185b8f46e4a075ea420f74ae9e8267dbf4
wittaya <w.imlukin@hotmail.com>
2025-10-07 16:44:35 +0700

    Ui Dashboard Charts

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,30 @@
 import DashboardContent from '@/features/dashboard/components/dashboard-content'
+import { getSalesData } from '@/features/dashboard/db/dashboard';
+import dayjs from '@/lib/dayjs'
 import React from 'react'
 
-const AdminPage = () => {
+const AdminPage = async () => {
+
+  const startDate = dayjs().subtract(1, "month").format("YYYY-MM-DD");
+  const endDate = dayjs().format("YYYY-MM-DD");
+
+
+const saleData = await getSalesData({
+  from: startDate,
+  to: endDate
+})
+
+
+
+
+
   return (
     <div className="p-4 sm:p-6 flex flex-col gap-6">
       <div className="flex flex-col gap-1">
         <h1 className="text-2xl sm:text-3xl font-bold">Dashboard</h1>
         <p  className="text-muted-foreground text-sm">Your store analytics and sales summary</p>
       </div>
-      <DashboardContent/>
+      <DashboardContent data={saleData}/>
     </div>
   )
 }
--- src/features/dashboard/components/dashboard-content.tsx
+++ src/features/dashboard/components/dashboard-content.tsx
@@ -1,12 +1,44 @@
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
-import React from 'react'
+'use client'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Bar, BarChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
-const DashboardContent = () => {
+interface DashboardContentProps {
+  data: Array<{
+    date: string;
+    revenue: number;
+    cost: number;
+    profit: number;
+  }>
+}
+
+
+
+const DashboardContent = ({data}:DashboardContentProps) => {
+
+
+
   return (
     <Card>
-        <CardHeader>
-            <CardTitle>Weekly sales for 1 month</CardTitle>
-        </CardHeader>
+      <CardHeader>
+        <CardTitle>Weekly sales for 1 month</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="h-[300px]">
+          <ResponsiveContainer>
+            <BarChart data={data}>
+              <XAxis dataKey="date" tick={{fontSize:15}}/>
+              <YAxis/>
+              <Legend/>
+              <Tooltip/>
+
+
+              <Bar dataKey="revenue" fill="#4F46E5" name="รายรับ (Revenue)" barSize={50}/>
+              <Bar dataKey="cost" fill="#E53E3E" name="ต้นทุน (Cost)" barSize={50}/>
+              <Bar dataKey="profit" fill="#22C55E" name="กำไร (Profit)" barSize={50}/>
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </CardContent>
     </Card>
   )
 }
